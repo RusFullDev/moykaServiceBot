c147a12e9d49eacb33bb8cacbc74a69547f00862
RusFullDev <urinrust@gmail.com>
2024-04-06 08:09:28 +0500

    moyka service bot

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function start() {
+  try {
+    const PORT = process.env.PORT || 3000;
+    const app = await NestFactory.create(AppModule);
+    await app.listen(PORT, () => {
+      console.log(`Server started at ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+start();
